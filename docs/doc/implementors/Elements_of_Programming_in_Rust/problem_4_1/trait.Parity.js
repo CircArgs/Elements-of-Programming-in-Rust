6b26885ad31e758298160c2e671a603365e38ea9
CircArgs <quebecname@gmail.com>
2020-12-28 13:05:36 -0500

    update readme

--- docs/doc/implementors/Elements_of_Programming_in_Rust/problem_4_1/trait.Parity.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["Elements_of_Programming_in_Rust"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
