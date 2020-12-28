ef77ae1d92cd561f38098d55ca6a53e11bfb2886
CircArgs <quebecname@gmail.com>
2020-12-28 12:22:34 -0500

    cargo doc straight to docs folder

---
+++ docs/doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["Elements_of_Programming_in_Rust"] = {"name":"","dirs":[{"name":"problem_4_1","files":["mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
