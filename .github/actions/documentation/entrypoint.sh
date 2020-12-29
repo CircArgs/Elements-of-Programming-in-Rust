80e17c8b3d57a16cf4c184a36cb8b00a7f233b9c
CircArgs <quebecname@gmail.com>
2020-12-28 21:35:06 -0500

    use ssh action

--- .github/actions/documentation/entrypoint.sh
+++ .github/actions/documentation/entrypoint.sh
@@ -3,12 +3,7 @@
 git config --global user.email ""
 git config --global user.name "CircArgs"
 
-eval "$(ssh-agent -s)"
-echo "-----BEGIN OPENSSH PRIVATE KEY-----" >> sshkey
-echo $DOCS_SSH >> sshkey
-echo "-----END OPENSSH PRIVATE KEY-----" >> sshkey
-chmod 400 sshkey
-ssh-add sshkey
+
 git clone --single-branch --branch docs git@github.com:CircArgs/Elements-of-Programming-in-Rust.git ./cloned
 cd cloned
 rm -rf docs
@@ -17,6 +12,7 @@ echo "generating docs"
 # creates documentation site in ./docs
 cargo doc --no-deps --document-private-items --target-dir ./docs
 echo "<head><meta http-equiv=\"refresh\" content=\"0; URL=./doc/Elements_of_Programming_in_Rust/index.html\" /></head><body>hello</body>" >> ./docs/index.html
+
 git add .
 git_hash=$(git rev-parse --short "$GITHUB_SHA")
 git commit -m "docs for commit hash $git_hash"
