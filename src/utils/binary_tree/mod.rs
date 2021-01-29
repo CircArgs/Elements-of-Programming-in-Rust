4c31d1cef46a8cf85a9f9ec1a03365a1b1cb22c2
CircArgs <quebecname@gmail.com>
2021-01-29 15:23:16 -0500

    binary tree inorder traversal in progress

--- src/utils/binary_tree/mod.rs
+++ src/utils/binary_tree/mod.rs
@@ -55,13 +55,65 @@ impl<T> BinaryTree<T> {
             self.root.as_mut().unwrap().insert(value);
         }
     }
+    pub fn inorder_iter<'a>(&'a self) -> InorderTraversal<'a, T> {
+        InorderTraversal {
+            parent: &self.root,
+            rel: NodeRelation::Left,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum NodeRelation {
+    Parent,
+    Left,
+    Right,
+}
+pub struct InorderTraversal<'a, T> {
+    parent: &'a Node<T>,
+    rel: NodeRelation,
+}
+
+impl<'a, T> Iterator for InorderTraversal<'a, T> {
+    type Item = &'a Box<TreeNode<T>>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.parent.is_none() {
+            return None;
+        } else {
+            match self.rel {
+                NodeRelation::Left => {
+                    let temp = self.parent.as_ref().unwrap().left.as_ref();
+                    self.rel = NodeRelation::Parent;
+                    if temp.is_none() {
+                        return self.next();
+                    } else {
+                        return temp;
+                    }
+                }
+                NodeRelation::Parent => {
+                    let temp = self.parent.as_ref();
+                    self.rel = NodeRelation::Right;
+                    return temp;
+                }
+                NodeRelation::Right => {
+                    let temp = self.parent.as_ref().unwrap().right.as_ref();
+                    self.rel = NodeRelation::Left;
+                    self.parent = &self.parent.as_ref().unwrap().left;
+                    if temp.is_none() {
+                        return self.next();
+                    } else {
+                        return temp;
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_insert() {
+    fn tree1() -> BinaryTree<i32> {
         let mut tree = BinaryTree::new();
         tree.insert(1);
         tree.insert(0);
@@ -69,6 +121,12 @@ mod tests {
         tree.insert(4);
         tree.insert(6);
         tree.insert(-2);
+        tree
+    }
+
+    #[test]
+    fn test_insert() {
+        let tree = tree1();
         assert_eq!(tree.root.as_ref().unwrap().data, 1);
         assert_eq!(tree.root.as_ref().unwrap().left.as_ref().unwrap().data, 0);
         assert_eq!(
@@ -111,5 +169,11 @@ mod tests {
                 .data,
             4
         );
+    }
+    #[test]
+    fn test_inorder_iter() {
+        let tree = tree1();
+        let comp = tree.inorder_iter().map(|e| e.data).collect::<Vec<_>>();
+        assert_eq!(comp, vec![0, 1, 5, -2, 4, 6]);
     }
 }
